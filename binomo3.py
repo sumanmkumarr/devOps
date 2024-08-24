f1a83b298ff73df2398f184492c662823ff29ed9
suman kumar <sumang91222@gmail.com>
2024-08-24 20:42:15 +0530

    code

--- binomo3.py
+++ binomo3.py
@@ -32,7 +32,7 @@ email_field.send_keys('email')
 password_field = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, '//*[@id="qa_auth_LoginPasswordInput"]/way-input/div/div/way-input-password/input'))  # Replace with actual XPath
 )
-password_field.send_keys(' pass ')
+password_field.send_keys('pass')
 
 # Submit the form
 password_field.send_keys(Keys.RETURN)
