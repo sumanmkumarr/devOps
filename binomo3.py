64986d30c69dc6a0eb8388605885674d35d1a596
suman kumar <sumang91222@gmail.com>
2024-08-27 21:57:49 +0530

    CHROME_OPTION

--- binomo3.py
+++ binomo3.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Set up Chrome options
+# Chrome options
 options = Options()
 options.add_argument("--start-maximized")
 
